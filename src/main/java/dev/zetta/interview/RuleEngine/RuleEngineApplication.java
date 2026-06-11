44fb87febddf394fea501c2fff9c60d4e673aaad
mnchv <mihail.minch.v@gmail.com>
2026-06-11 16:20:10 +0300

    First commit

---
+++ src/main/java/dev/zetta/interview/RuleEngine/RuleEngineApplication.java
@@ -0,0 +1,13 @@
+package dev.zetta.interview.RuleEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RuleEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zetta/interview/RuleEngine/RuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zetta.interview.RuleEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
